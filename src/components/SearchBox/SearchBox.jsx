140821627bac17c693669e88a0c678e1dec7e199
ValiaStebivka <valia.stebivka2016@gmail.com>
2024-03-05 11:59:52 +0200

    add files + regux folder

---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,17 @@
+import { useDispatch } from "react-redux";
+import css from "./SearchBox.module.css";
+import { setFilter } from "../../redux/filtersSlice";
+
+export const SearchBox = () => {
+  const dispatch = useDispatch();
+  const handleSearch = (e) => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <div className={css.searchBox}>
+      <label htmlFor="searchBox">Find contacts by name</label>
+      <input type="text" id="searchBox" onChange={handleSearch} />
+    </div>
+  );
+};
